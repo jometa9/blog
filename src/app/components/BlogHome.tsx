f22d96f7fc7b1094a3c5e9a76328a1d32ea2d2c6
483343434 <omolalehusman@gmail.com>
2024-09-19 08:20:22 -0300

    rebrand

--- src/app/components/BlogHome.tsx
+++ src/app/components/BlogHome.tsx
@@ -20,24 +20,40 @@ const BlogHome: React.FC<SearchablePostsProps> = ({ posts = [] }) => {
 
   return (
     <>
-      <p>Documenting the journey</p>
+      <p>Hey there! 👋 I'm Joaquin Metayer,</p>
+      <p><b>React developer</b> sharing the journey.</p>
+      <p>Now, focusing on AWS Cloud.</p>
       <p>
+        Creating on{" "}
+        <a target="_blank" href="https://github.com/joaquinmetayer/">
+          GitHub
+        </a>{" "}
+        and working at{" "}
+        <a target="_blank" href="https://www.instagram.com/bitlogic.io/">
+          @bitlogic.io
+        </a>
+        .
+      </p>
+      <p>
+        Find me on{" "}
         <a href="https://www.linkedin.com/in/joaquinmetayer/" target="_blank">
           LinkedIn
-        </a>{" "}
+        </a>
+        ,{" "}
         <a href="https://github.com/joaquinmetayer" target="_blank">
           GitHub
         </a>
+        , or{" "}
+        <a target="_blank" href="mailto:joaquinmetayer@gmail.com">
+          email
+        </a>.
       </p>
       <p>Total posts: {posts.length}</p>
-      <hr />
       <input
-        placeholder="Search posts"
+        placeholder="Search posts here"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <p></p>
-      <hr />
       {postPreviews}
     </>
   );
