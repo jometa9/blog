e46cb425474d2d2615ad35554a1aebf324133a32
jometa <joaquinmetayer@gmail.com>
2024-11-30 15:09:28 -0300

    lesgo

--- src/app/components/HomeDeveloper.tsx
+++ src/app/components/HomeDeveloper.tsx
@@ -3,14 +3,7 @@ import Link from "next/link";
 export default function HomeDeveloper() {
   return (
     <>
-      <img
-        src="/images/IMG_4718.png"
-        alt="laptop"
-        style={{ marginTop: "10px", maxWidth: "300px" }}
-      />
-      <h2>Joaquin Metayer</h2>
-      <p>ReactJS Developer building for the internet.</p>
-      <p>Minimalism as a philosophy, simplicity as a style.</p>
+      <h2>jometayer</h2>
       <ul>
         <li>
           <p>
@@ -26,6 +19,13 @@ export default function HomeDeveloper() {
           <p>
             <a href="https://www.youtube.com/@jometayer" target="_blank">
               YouTube
+            </a>
+          </p>
+        </li>
+        <li>
+          <p>
+            <a href="https://www.instagram.com/jometayer" target="_blank">
+              Instagram
             </a>
           </p>
         </li>
