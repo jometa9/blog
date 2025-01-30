1accaccd55e7bd07637fc461267cdd83a721748c
jometa <joaquinmetayer@gmail.com>
2025-01-30 00:25:25 -0300

    remove commented-out links from HomeLinks component

--- src/app/components/HomeLinks.tsx
+++ src/app/components/HomeLinks.tsx
@@ -4,6 +4,7 @@ export default function HomeLinks() {
   return (
     <div className="homeLinksContainer">
       <h1 className="homeLinksTitle">jometa</h1>
+      {/*
       <div className="homeLinksLinkContainer">
         <a
           href="https://www.linkedin.com/in/joaquinmetayer"
@@ -13,29 +14,14 @@ export default function HomeLinks() {
           LinkedIn
         </a>
         <a
-          href="https://www.cf3funded.com"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          CF3
-        </a>
-        <a
           href="https://www.instagram.com/jometa_"
           target="_blank"
           className="homeLinksLink"
         >
           Instagram
         </a>
-        {/*
-        <a
-          href="https://x.com/_jometa"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          Twitter
-        </a>
-        */}
       </div>
+      */}
     </div>
   );
 }
