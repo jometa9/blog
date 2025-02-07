45cf16d58787b5853d26cf6724c35c374802bd92
jometa <joaquinmetayer@gmail.com>
2025-02-06 22:24:54 -0300

    2224

--- src/app/components/HomeLinks.tsx
+++ src/app/components/HomeLinks.tsx
@@ -1,37 +1,14 @@
-import Link from "next/link";
-
 export default function HomeLinks() {
   return (
     <div className="homeLinksContainer">
       <h1 className="homeLinksTitle">jometa</h1>
       <div className="homeLinksLinkContainer">
         <a
-          href="https://www.cf3funded.com"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          CF3
-        </a>
-        <a
-          href="https://www.instagram.com/cf3dev"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          @CF3Dev
-        </a>
-        <a
           href="https://www.instagram.com/jometa_"
           target="_blank"
           className="homeLinksLink"
         >
-          @jometa_
-        </a>
-        <a
-          href="https://github.com/jometa9"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          GitHub
+          Instagram
         </a>
         <a
           href="https://www.linkedin.com/in/joaquinmetayer"
