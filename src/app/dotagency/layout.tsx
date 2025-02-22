07f97c8a37559346d0fb1fc5e64c6e319cf8c4b9
jometa <joaquinmetayer@gmail.com>
2025-02-22 15:05:32 -0300

    dotAgency

---
+++ src/app/dotagency/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+    title: "dotAgency",
+    icons: {
+      icon: "/dotagency-favicon.ico",
+    },
+  };
+  
+  export default function DotAgencyLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <div>
+        {children}
+      </div>
+    );
+  }
+  
