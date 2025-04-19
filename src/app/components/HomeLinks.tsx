f1f363a9101ca0f6c538ac37dfb1bfaebdb27d47
jometa <joaquinmetayer@gmail.com>
2025-04-18 23:21:33 -0300

    2321

--- src/app/components/HomeLinks.tsx
+++ src/app/components/HomeLinks.tsx
@@ -25,7 +25,7 @@ export default function HomeLinks() {
           LinkedIn
         </a>
       </div>
-      <p className="homeQuote">building the future</p>
+      <p className="homeQuote">growth log</p>
     </div>
   );
 }
