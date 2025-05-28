6e2c1aef3ba1ca2e86a6acc9eb44f70e474a0609
jometa <joaquinmetayer@gmail.com>
2025-05-28 13:11:03 -0300

    feat: remove Instagram and YouTube links from HomeLinks component

--- src/app/components/HomeLinks.tsx
+++ src/app/components/HomeLinks.tsx
@@ -8,20 +8,6 @@ export default function HomeLinks({ totalPosts = 0 }: HomeLinksProps) {
       <h1 className="homeLinksTitle">jometa</h1>
       <div className="homeLinksLinkContainer">
         <a
-          href="https://www.instagram.com/jometa_"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          Instagram
-        </a>
-        <a
-          href="https://www.youtube.com/@joaquinmetayer"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          YouTube
-        </a>
-        <a
           href="https://www.linkedin.com/in/joaquinmetayer"
           target="_blank"
           className="homeLinksLink"
