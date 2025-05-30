2432524b79a91d2d946b6c4932965ad28777c99f
jometa <joaquinmetayer@gmail.com>
2025-05-30 07:51:21 -0300

    feat: remove social media links from HomeLinks component

--- src/app/components/HomeLinks.tsx
+++ src/app/components/HomeLinks.tsx
@@ -6,29 +6,6 @@ export default function HomeLinks({ totalPosts = 0 }: HomeLinksProps) {
   return (
     <div className="homeLinksContainer">
       <h1 className="homeLinksTitle">jometa</h1>
-      <div className="homeLinksLinkContainer">
-        <a
-          href="https://www.linkedin.com/in/joaquinmetayer"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          LinkedIn
-        </a>
-        <a
-          href="https://www.instagram.com/jometa_/"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          Instagram
-        </a>
-        <a
-          href="https://www.youtube.com/@joaquinmetayer"
-          target="_blank"
-          className="homeLinksLink"
-        >
-          Youtube
-        </a>
-      </div>
       <p className="homeQuote">{totalPosts} posts</p>
     </div>
   );
