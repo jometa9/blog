acb054ba0dc08635e7ca2e5a9b85d2eb04493adf
jometa <joaquinmetayer@gmail.com>
2025-05-30 12:18:26 -0300

    feat: update HomeLinks component to include LinkedIn link and modify homeQuote styling

--- src/app/components/HomeLinks.tsx
+++ src/app/components/HomeLinks.tsx
@@ -6,7 +6,35 @@ export default function HomeLinks({ totalPosts = 0 }: HomeLinksProps) {
   return (
     <div className="homeLinksContainer">
       <h1 className="homeLinksTitle">jometa</h1>
+      <div>
+        <p className="homeLinksLinkContainer">
+          <a
+            href="https://www.instagram.com/jometa_/"
+            target="_blank"
+            className="homeLinksLink"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.youtube.com/@joaquinmetayer"
+            target="_blank"
+            className="homeLinksLink"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.linkedin.com/in/joaquinmetayer"
+            target="_blank"
+            className="homeLinksLink"
+          >
+            LinkedIn
+          </a>
+        </p>
+      </div>
       <p className="homeQuote">{totalPosts} posts</p>
+      <p className="homeQuote">
+        fixing internet problems
+      </p>
     </div>
   );
 }
